52ae0e2fcba31239ce10d1c0c4502b72c21a29b0
Joseph Bielawski <stloyd@gmail.com>
2014-01-22 22:23:12 +0100

    Replace code to allow usage against PHP 5.3

--- tests/Negotiation/Tests/Stack/NegotiationTest.php
+++ tests/Negotiation/Tests/Stack/NegotiationTest.php
@@ -13,9 +13,9 @@ class NegotiationTest extends TestCase
     public function testAcceptHeader()
     {
         $app = $this->createStackedApp();
-        $req = $this->createRequest(null, [
+        $req = $this->createRequest(null, array(
             'Accept' => 'application/json',
-        ]);
+        ));
 
         $app->handle($req);
 
@@ -29,9 +29,9 @@ class NegotiationTest extends TestCase
     public function testAcceptLanguageHeader()
     {
         $app = $this->createStackedApp();
-        $req = $this->createRequest(null, [
+        $req = $this->createRequest(null, array(
             'Accept-Language' => 'en; q=0.1, fr; q=0.4, fu; q=0.9, de; q=0.2',
-        ]);
+        ));
 
         $app->handle($req);
 
@@ -47,9 +47,9 @@ class NegotiationTest extends TestCase
     public function testDecodeBody($method, $content, $mimeType, $expected)
     {
         $app = $this->createStackedApp();
-        $req = $this->createRequest($content, [
+        $req = $this->createRequest($content, array(
             'Content-Type' => $mimeType,
-            ]);
+        ));
         $req->setMethod($method);
 
         $app->handle($req);
@@ -59,24 +59,24 @@ class NegotiationTest extends TestCase
 
     public function dataProviderForTestDecodeBody()
     {
-        return [
-            [ 'POST', 'foo', 'application/json', [] ],
-            [ 'POST', '<response><foo>bar</foo></response>', 'application/xml', [ 'foo' => 'bar' ] ],
-            [ 'POST', '{ "foo": "bar" }', 'application/json', [ 'foo' => 'bar' ] ],
-            [ 'PUT', '', 'application/json', [] ],
-            [ 'GET', '{ "foo": "bar" }', 'application/json', [] ],
-            [ 'GET', '<response><foo>bar</foo></response>', 'application/xml', [] ],
-        ];
+        return array(
+            array('POST', 'foo', 'application/json', array()),
+            array('POST', '<response><foo>bar</foo></response>', 'application/xml', array('foo' => 'bar')),
+            array('POST', '{ "foo": "bar" }', 'application/json', array('foo' => 'bar')),
+            array('PUT', '', 'application/json', array()),
+            array('GET', '{ "foo": "bar" }', 'application/json', array()),
+            array('GET', '<response><foo>bar</foo></response>', 'application/xml', array()),
+        );
     }
 
-    private function createStackedApp(array $responseHeaders = [])
+    private function createStackedApp(array $responseHeaders = array())
     {
         return new Negotiation(new MockApp($responseHeaders));
     }
 
-    private function createRequest($content = null, array $requestHeaders = [])
+    private function createRequest($content = null, array $requestHeaders = array())
     {
-        $request = new Request([], [], [], [], [], [], $content);
+        $request = new Request(array(), array(), array(), array(), array(), array(), $content);
         $request->headers->add($requestHeaders);
 
         return $request;
